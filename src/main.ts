68bceb63fbeb753d65b6a32c470cfcf3b55ced79
enzotorrs <enzotorres969@gmail.com>
2025-02-10 20:08:23 -0300

    feat: add validation filter and pipes

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { SequelizeValidationFilter } from './core/filters/validation.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new SequelizeValidationFilter());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('Ceos API')
     .setVersion('dev')
     .addTag('assets')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory); await app.listen(process.env.PORT ?? 3000);
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
