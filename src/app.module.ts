bf16ea28457e5836a535d9c53ebf685c7f6e2393
enzotorrs <enzotorres969@gmail.com>
2025-02-10 14:48:57 -0300

    feat: add database and asset module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AssetModule } from './asset/asset.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [AssetModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, AssetModule],
 })
 export class AppModule {}
