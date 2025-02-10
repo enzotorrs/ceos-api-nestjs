0f835cb1123c962c7e9cc2cac0f780fee3378f27
enzotorrs <enzotorres969@gmail.com>
2025-02-10 14:34:13 -0300

    feat: add database module to db connection

---
+++ src/database/database.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'ceos',
+      autoLoadModels: true,
+      synchronize: true,
+      logging: false,
+    }),
+  ],
+})
+export class DatabaseModule {}
