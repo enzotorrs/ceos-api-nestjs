c26099fecf4b3749fff2fa5283e4101b6ee69c86
enzotorrs <enzotorres969@gmail.com>
2025-02-20 16:42:40 -0300

    feat: add interceptor to get requets metrics

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { AssetModule } from './asset/asset.module';
 import { DatabaseModule } from './database/database.module';
 import { CoreModule } from './core/core.module';
 import { UploadModule } from './upload/upload.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { RequestsMetricsInterceptor } from './core/routeMetrics.interceptor';
 
 @Module({
   imports: [DatabaseModule, AssetModule, CoreModule, UploadModule],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: RequestsMetricsInterceptor
+    },
+  ],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/core/routeMetrics.interceptor.ts
@@ -0,0 +1,48 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { meterProvider } from 'src/tracing';
+
+@Injectable()
+export class RequestsMetricsInterceptor implements NestInterceptor {
+  private activeRequestsMetric;
+
+  constructor() {
+    const meter = meterProvider.getMeter('nestjs-app');
+    this.activeRequestsMetric = meter.createCounter('http_requests', {
+      description: 'Number of HTTP requests',
+    });
+  }
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest();
+    return next.handle().pipe(
+      tap({
+        next: (response) => {
+          const labels = {
+            service_name: "ceos-api",
+            method: request.method,
+            route: request.url,
+            status_code: response.statusCode,
+          };
+          this.activeRequestsMetric.add(1, labels); 
+        },
+        error: (err) => {
+          const labels = {
+            service_name: "ceos-api",
+            method: request.method,
+            route: request.url,
+            status_code: err?.status || 500,
+            error: true
+          };
+          this.activeRequestsMetric.add(1, labels);
+        },
+      }),
+    );
+  }
+}
